0bad1137d23753dc24717ac6b24f24d003266a55
cjamison <elocjam13@gmail.com>
2025-12-26 20:15:34 -0500

    created main application, created database properties and main database location

---
+++ backend/src/main/java/com/notapos/NotaPosApplication.java
@@ -0,0 +1,22 @@
+package com.notapos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Nota-POS Main Application
+ * 
+ * Restaurant Point of Sale System
+ * Built by Cole Jamison
+ * 
+ * @author CJ
+ * @version 1.0.0
+ */
+
+@SpringBootApplication
+public class NotaPosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotaPosApplication.class, args);
+    }
+}
